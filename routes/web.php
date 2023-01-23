3ad968323caaeb5126538b344f595d3208b2af06
afnansafdar23 <afnan.centosquare@gmail.com>
2023-01-23 11:46:22 +0500

    Spatie Media Library And File Manager

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\FileManagerController;
 use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UploadController;
 use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -23,7 +26,7 @@ Route::view('/', 'welcome');
 Route::controller(AuthController::class)
     ->prefix('auth')
     ->name('auth.')
-    ->group(function(){
+    ->group(function () {
         Route::get('login',  'loginView')->name('login');
         Route::post('login-user', 'userLogin')->name('login.user');
         Route::get('register',  'registerView')->name('register');
@@ -31,40 +34,48 @@ Route::controller(AuthController::class)
         Route::get('logout', 'logout')->name('logout');
     });
 
+Route::middleware('auth')->group(function () {
+    Route::controller(UserController::class)
+        ->prefix('user')
+        ->name('user.')
+        ->group(function () {
+            Route::get('index', 'index')->name('index');
+            Route::get('create', 'create')->name('create');
+            Route::post('store', 'store')->name('store');
+            Route::get('edit/{user}', 'edit')->name('edit');
+            Route::post('update/{user}', 'update')->name('update');
+            Route::get('delete/{user}', 'destroy')->name('delete');
+        });
 
-Route::controller(UserController::class)
-    ->prefix('user')
-    ->name('user.')
-    ->group(function () {
-        Route::get('index', 'index')->name('index');
-        Route::get('create', 'create')->name('create');
-        Route::post('store', 'store')->name('store');
-        Route::get('edit/{user}', 'edit')->name('edit');
-        Route::post('update/{user}', 'update')->name('update');
-        Route::get('delete/{user}', 'destroy')->name('delete');
-    });
+    Route::get('filemanager', [FileManagerController::class, 'index'])->name('file.index');
+    Route::post('filemanager/upload', [FileManagerController::class, 'upload'])->name('file.upload');
+    Route::post('file/store', [FileManagerController::class, 'store'])->name('file.store');
+    Route::get('/file/get-image/{id}', [FileManagerController::class, 'getImage']);
+    Route::delete('filemanager/{file}', [FileManagerController::class, 'delete'])->name('filemanager.delete');
 
-Route::controller(RoleController::class)
-    ->prefix('role')
-    ->name('role.')
-    ->group(function () {
-        Route::get('index', 'index')->name('index');
-        Route::get('create', 'create')->name('create');
-        Route::post('store', 'store')->name('store');
-        Route::get('edit/{role}', 'edit')->name('edit');
-        Route::post('update/{role}', 'update')->name('update');
-        Route::get('delete/{role}', 'destroy')->name('delete');
-    });
 
-Route::controller(PermissionController::class)
-    ->prefix('permission')
-    ->name('permission.')
-    ->group(function () {
-        Route::get('index', 'index')->name('index');
-        Route::get('create', 'create')->name('create');
-        Route::post('store', 'store')->name('store');
-        Route::get('edit/{permission}', 'edit')->name('edit');
-        Route::post('update{permission}', 'update')->name('update');
-        Route::get('delete/{permission}', 'destroy')->name('delete');
-        Route::get('synchronize', 'synchronize')->name('synchronize');
-    });
+    Route::controller(RoleController::class)
+        ->prefix('role')
+        ->name('role.')
+        ->group(function () {
+            Route::get('index', 'index')->name('index');
+            Route::get('create', 'create')->name('create');
+            Route::post('store', 'store')->name('store');
+            Route::get('edit/{role}', 'edit')->name('edit');
+            Route::post('update/{role}', 'update')->name('update');
+            Route::get('delete/{role}', 'destroy')->name('delete');
+        });
+
+    Route::controller(PermissionController::class)
+        ->prefix('permission')
+        ->name('permission.')
+        ->group(function () {
+            Route::get('index', 'index')->name('index');
+            Route::get('create', 'create')->name('create');
+            Route::post('store', 'store')->name('store');
+            Route::get('edit/{permission}', 'edit')->name('edit');
+            Route::post('update{permission}', 'update')->name('update');
+            Route::get('delete/{permission}', 'destroy')->name('delete');
+            Route::get('synchronize', 'synchronize')->name('synchronize');
+        });
+});
